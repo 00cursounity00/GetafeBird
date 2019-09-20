59cd244225297b67c40aa54865abcb74e8cd7c99
00cursounity00 <00cursounity00@gmail.com>
2019-09-20 13:55:55 +0200

    Sesion 2019-09-20

---
+++ Assets/_GameObjects/Scripts/Pajaro.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pajaro : MonoBehaviour
+{
+    Rigidbody rb;
+    int fuerza = 300;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Saltar();
+        }
+    }
+
+    void Saltar()
+    {
+        rb.AddForce(Vector3.up * fuerza);
+    }
+}
---
+++ Assets/_GameObjects/Scripts/SpawnerTuberias.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerTuberias : MonoBehaviour
+{
+    public GameObject prefabTuberia;
+    float ratio = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {        
+        InvokeRepeating("SpawnTuberia", 0, ratio);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void SpawnTuberia ()
+    {
+        Instantiate(prefabTuberia, transform);
+    }
+}
